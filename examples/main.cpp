4e06bb7f482dda38a058cdf42b78886e73693be1
Tomasz Siekierda <sierdzio@gmail.com>
2020-08-06 22:24:54 +0200

    Prepare blocking label - WIP test for proper widget painting (currently failing, of course).

--- examples/main.cpp
+++ examples/main.cpp
@@ -17,8 +17,13 @@ int main(int argc, char *argv[])
     label.setTextColor(Terminal::Color::Green);
     label.show();
 
+    Tg::Label labelAbove(QObject::tr("Block!"), &label);
+    labelAbove.setPosition(QPoint(40, 0));
+    labelAbove.setTextColor(Terminal::Color::Cyan);
+    labelAbove.show();
+
     Tg::ReversibleAnimation animation(&label, "position");
-    animation.setDuration(8000);
+    animation.setDuration(5000);
     animation.setStartValue(QPoint(0, 0));
     //qDebug() << "Screen size" << screen.size();
     const int endX = screen.size().width() - label.text().size();
