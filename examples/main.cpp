794410d9747e51665b16282a6c8d053331b57c6e
Tomasz Siekierda <sierdzio@gmail.com>
2020-08-07 14:41:51 +0200

    Add label below - for testing.

--- examples/main.cpp
+++ examples/main.cpp
@@ -12,12 +12,17 @@ int main(int argc, char *argv[])
     QCoreApplication app(argc, argv);
 
     Tg::Screen screen;
-    Tg::Label label(QObject::tr("Hello world!"), &screen);
-    label.setPosition(QPoint(5, 0));
+    Tg::Label labelBelow(QObject::tr("Below"), &screen);
+    labelBelow.setPosition(QPoint(20, 0));
+    labelBelow.setTextColor(Terminal::Color::Red);
+    labelBelow.show();
+
+    Tg::Label label(QObject::tr("Hello world!"), &labelBelow);
+    label.setPosition(QPoint(0, 0));
     label.setTextColor(Terminal::Color::Green);
     label.show();
 
-    Tg::Label labelAbove(QObject::tr("Block!"), &label);
+    Tg::Label labelAbove(QObject::tr("Above"), &label);
     labelAbove.setPosition(QPoint(40, 0));
     labelAbove.setTextColor(Terminal::Color::Cyan);
     labelAbove.show();
--- widgets/tglabel.cpp
+++ widgets/tglabel.cpp
@@ -26,26 +26,6 @@ QString Tg::Label::text() const
 
 QString Tg::Label::drawPixel(const QPoint &pixel) const
 {
-//    if (visible()) {
-//        Tg::TextStream stream(stdout);
-
-//        // TODO: do not clear here, but in screen painter!
-//        stream << Commands::clear;
-
-//        if (position().x() > 0 || position().y() > 0) {
-//            stream << Commands::moveToPosition(position().x(), position().y());
-//        }
-
-//        stream << Terminal::colorCode(textColor());
-//        if (boundingRectangle().right() > screen()->size().width()) {
-//            QString clippedText = text();
-//            clippedText.chop(boundingRectangle().right() - screen()->size().width());
-//        } else {
-//            stream << text();
-//        }
-//        stream << Colors::end;
-//    }
-
     QString result;
     result.append(QString::fromStdString(Terminal::colorCode(textColor())));
     const int charPosition = pixel.x() - position().x();
@@ -65,7 +45,7 @@ void Tg::Label::setText(const QString &text)
     if (current.width() != _text.length()) {
         current.setWidth(text.length());
         // TODO: multiline labels!
-        current.setHeight(1);
+        current.setHeight(_text.isEmpty()? 0 : 1);
         setSize(current);
     }
 
