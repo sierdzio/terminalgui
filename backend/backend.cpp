e8a9d9376adae4d50a812991872a38660d522f95
Tomasz Siekierda <tsiekierda@milosolutions.com>
2020-09-05 21:37:28 +0200

    Separate Linux-specific part of backend into backend_linux.cpp.

--- backend/backend.cpp
+++ backend/backend.cpp
@@ -1,30 +1,6 @@
 #include "backend.h"
 
-// For reading terminal size
-// https://stackoverflow.com/questions/1022957/getting-terminal-width-in-c
-#include <sys/ioctl.h>
-#include <stdio.h>
-#include <unistd.h>
 
-Terminal::Size Terminal::updateSize()
-{
-    winsize w;
-    ioctl(STDOUT_FILENO, TIOCGWINSZ, &w);
-    Terminal::Size result;
-    result.width = w.ws_col;
-    result.height = w.ws_row;
-    return result;
-}
-
-Terminal::Position Terminal::currentPosition()
-{
-    winsize w;
-    ioctl(STDOUT_FILENO, TIOCGWINSZ, &w);
-    Terminal::Position result;
-    result.x = w.ws_xpixel;
-    result.y = w.ws_ypixel;
-    return result;
-}
 
 std::string Terminal::colorCode(const Terminal::Color4Bit foregroundColor,
                                 const Terminal::Color4Bit backgroundColor)
@@ -44,30 +20,4 @@ std::string Terminal::colorEnd()
 std::string Terminal::Command::moveToPosition(const int x, const int y)
 {
     return "\033[" + std::to_string(y) + ";" + std::to_string(x) + "H";
-}
-
-int Terminal::keyboardBufferSize()
-{
-    int i;
-    ioctl(standardInputIndex, FIONREAD, &i);
-    return i;
-}
-
-// TODO: use termios! Right?
-//#include <termios.h>
-
-Terminal::RawTerminalLocker::RawTerminalLocker()
-{
-    //struct termios term;
-    //tcgetattr(standardInputIndex, &term);
-    //term.c_lflag &= ~ICANON;
-    //tcsetattr(standardInputIndex, TCSANOW, &term);
-    //setbuf(stdin, NULL);
-
-    system("stty raw -echo");
-}
-
-Terminal::RawTerminalLocker::~RawTerminalLocker()
-{
-    system("stty cooked echo");
 }
---
+++ backend/backend_linux.cpp
@@ -0,0 +1,53 @@
+#include "backend.h"
+
+// For reading terminal size
+// https://stackoverflow.com/questions/1022957/getting-terminal-width-in-c
+#include <sys/ioctl.h>
+#include <stdio.h>
+#include <unistd.h>
+
+Terminal::Size Terminal::updateSize()
+{
+    winsize w;
+    ioctl(STDOUT_FILENO, TIOCGWINSZ, &w);
+    Terminal::Size result;
+    result.width = w.ws_col;
+    result.height = w.ws_row;
+    return result;
+}
+
+Terminal::Position Terminal::currentPosition()
+{
+    winsize w;
+    ioctl(STDOUT_FILENO, TIOCGWINSZ, &w);
+    Terminal::Position result;
+    result.x = w.ws_xpixel;
+    result.y = w.ws_ypixel;
+    return result;
+}
+
+int Terminal::keyboardBufferSize()
+{
+    int i;
+    ioctl(standardInputIndex, FIONREAD, &i);
+    return i;
+}
+
+// TODO: use termios! Right?
+//#include <termios.h>
+
+Terminal::RawTerminalLocker::RawTerminalLocker()
+{
+    //struct termios term;
+    //tcgetattr(standardInputIndex, &term);
+    //term.c_lflag &= ~ICANON;
+    //tcsetattr(standardInputIndex, TCSANOW, &term);
+    //setbuf(stdin, NULL);
+
+    system("stty raw -echo");
+}
+
+Terminal::RawTerminalLocker::~RawTerminalLocker()
+{
+    system("stty cooked echo");
+}
