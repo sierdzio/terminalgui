89de82df45599ab2fa09b358fd86d1f56f5423c4
Tomasz Siekierda <sierdzio@gmail.com>
2020-10-03 16:19:52 +0200

    Preparing list view example.

---
+++ examples/item-views-example.cpp
@@ -0,0 +1,35 @@
+#include <QCoreApplication>
+#include <QDebug>
+
+#include <tgscreen.h>
+#include <widgets/tgwidget.h>
+#include <widgets/tglabel.h>
+#include <widgets/tgbutton.h>
+
+int main(int argc, char *argv[])
+{
+    QCoreApplication app(argc, argv);
+
+    Terminal::RawTerminalLocker locker;
+
+    const QSize size(20, 15);
+
+    Tg::Screen screen;
+    Tg::Widget widget(&screen);
+    widget.setObjectName("widget");
+    widget.setPosition(QPoint(1, 1));
+    widget.setSize(size);
+    widget.setLayoutType(Tg::Layout::Type::Column);
+    widget.show();
+
+    Tg::Button quitButton(QObject::tr("Quit"), &widget);
+    quitButton.setObjectName("quitButton");
+    quitButton.show();
+
+    // TODO: add example scroll bar, scroll area, list view etc.
+
+    CHECK(QObject::connect(&quitButton, &Tg::Button::clicked,
+                           &app, &QCoreApplication::quit));
+
+    return app.exec();
+}
