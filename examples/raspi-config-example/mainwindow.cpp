a0b566e953ac5be83b6ab504943876343d1adb6c
Tomasz Siekierda <sierdzio@gmail.com>
2021-03-20 18:16:48 +0100

    WIP main page of raspi config.

--- examples/raspi-config-example/mainwindow.cpp
+++ examples/raspi-config-example/mainwindow.cpp
@@ -1,16 +1,29 @@
 #include "mainwindow.h"
 
 #include <widgets/tgbutton.h>
+#include <widgets/tglistview.h>
 
 #include <QCoreApplication>
+#include <QStringListModel>
 
 MainWindow::MainWindow(Tg::Screen *screen) : Tg::Widget(screen)
 {
     setTitle(QObject::tr("Raspberry Pi Configuration Tool (raspi-config-tg)"));
+    setLayoutType(Tg::Layout::Type::Column);
+    setBackgroundColor(Tg::Color::Predefined::Gray);
 
-    _button = new Tg::Button(tr("Button"), this);
+    _listView = new Tg::ListView(this);
+    _listView->setModel(new QStringListModel(_mainMenuLabels, _listView));
+    _listView->setSize(QSize(size().width(), 6));
+    _listView->show();
 
-    CHECK(connect(_button, &Tg::Button::clicked,
+    _finishButton = new Tg::Button(tr("Finish"), this);
+    _finishButton->setTextColor(Tg::Color::Predefined::White);
+    _finishButton->setBackgroundColor(Tg::Color::Predefined::Black);
+    _finishButton->setSize(QSize(6, 1));
+    _finishButton->show();
+
+    CHECK(connect(_finishButton, &Tg::Button::clicked,
                   QCoreApplication::instance(), &QCoreApplication::quit));
 
     show();
--- examples/raspi-config-example/mainwindow.h
+++ examples/raspi-config-example/mainwindow.h
@@ -5,6 +5,7 @@
 
 namespace Tg {
 class Button;
+class ListView;
 }
 
 class MainWindow : public Tg::Widget
@@ -15,7 +16,19 @@ public:
     MainWindow(Tg::Screen *screen);
 
 private:
-    Tg::Button *_button = nullptr;
+    Tg::ListView *_listView = nullptr;
+    Tg::Button *_finishButton = nullptr;
+
+    const QStringList _mainMenuLabels = {
+        tr("System Options"),
+        tr("Display Options"),
+        tr("Interface Options"),
+        tr("Performance Options"),
+        tr("Localisation Options"),
+        tr("Advanced Options"),
+        tr("Update"),
+        tr("About raspi-config-tg")
+    };
 };
 
 #endif // MAINWINDOW_H
